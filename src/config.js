dfee26d7e73800b3b952e6e3d99c16258d5ea46d
Boggiefottie <104435306+Boggiefottie@users.noreply.github.com>
2023-06-01 02:22:19 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = "[YOUR ADDRESS HERE]"; 
+export const ROUTER_ADDRESS = "0x959953770D2B93334B536b2713621a70e370ddC5"; 
 
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]: "[YOUR URL HERE]",
+    [Goerli.chainId]: "https://eth-goerli.g.alchemy.com/v2/XY00pG4vFMcboUI6fVNXyAqIdLTjzcnx",
   },
 };
